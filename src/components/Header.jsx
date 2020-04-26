bf3c151114f97cf888f712e6838674f841d64b5c
abobkerelaghel <abobkerelaghel@gmail.com>
2020-04-27 00:37:33 +0100

    *

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 const Header = () => (
     <header>
         <h1>ExpensifyApp</h1>
-        <p> <NavLink activeClassName="is-active-link" exact to="/">Home Page ma Nigga</NavLink></p>
-        <p> <NavLink activeClassName="is-active-link" to="/create">Add Page ma Nigga</NavLink></p>
-        <p> <NavLink activeClassName="is-active-link" to="/edit">Edit Page ma Nigga</NavLink></p>
-        <p> <NavLink activeClassName="is-active-link" to="/help">Help Page ma Nigga</NavLink></p>
+        <p> <NavLink activeClassName="is-active-link" exact to="/">Home Page </NavLink></p>
+        <p> <NavLink activeClassName="is-active-link" to="/create">Add Page </NavLink></p>
+        <p> <NavLink activeClassName="is-active-link" to="/edit">Edit Page </NavLink></p>
+        <p> <NavLink activeClassName="is-active-link" to="/help">Help Page </NavLink></p>
     </header>
 );
 
