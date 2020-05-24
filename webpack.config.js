0561dde4ec64138f6701fcedbdfeea3025a50751
abobkerelaghel <abobkerelaghel@gmail.com>
2020-05-24 08:21:35 +0100

    complated the setup of webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = env => {
     const isProduction = env === "production";
 
     return {
         entry: './src/app.jsx',
+        optimization: {
+            minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+        },
         output: {
             path: path.join(__dirname,'public'),
             filename: "bundle.js"
         },
         mode: "development",
+        plugins: [new MiniCssExtractPlugin({
+            filename: "style.css"
+        })],
         module: {
             rules: [{
                 loader: 'babel-loader',
@@ -17,10 +26,25 @@ module.exports = env => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: !isProduction
+                    }
+                }, {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }]
             }]
         },
-        devtool: isProduction ? "source-map" : "cheap-module-source-map",
+        devtool: isProduction ? "source-map" : "inline-source-map",
         devServer: {
             contentBase: path.join(__dirname,'public'),
             historyApiFallback: true
