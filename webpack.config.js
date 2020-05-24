8dfacfe395795b745d71f4d9210f17176346147b
abobkerelaghel <abobkerelaghel@gmail.com>
2020-05-24 02:26:17 +0100

    before applying the css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
 const path = require('path');
-const src = "./src/app.jsx";
 
-module.exports = {
-    entry: './src/app.jsx',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: "bundle.js"
-    },
-    mode: "development",
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.jsx$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: "cheap-module-source-map",
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-    }
+module.exports = env => {
+    const isProduction = env === "production";
+
+    return {
+        entry: './src/app.jsx',
+        output: {
+            path: path.join(__dirname,'public'),
+            filename: "bundle.js"
+        },
+        mode: "development",
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.jsx$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devtool: isProduction ? "source-map" : "cheap-module-source-map",
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true
+        }
+    };
 };
