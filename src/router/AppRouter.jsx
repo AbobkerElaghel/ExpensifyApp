5ae815d3d2494448a3fa1a0f3f9f173aa723a616
AbobkerElaghel <abobkerelaghel@gmail.com>
2020-06-28 13:13:22 +0100

    dell

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,14 +6,16 @@ import AddExpensePage from "../components/AddExpensePage.jsx";
 import EditExpensePage from "../components/EditExpensePage.jsx";
 import HelpPage from "../components/HelpPage.jsx";
 import NotFoundPage from "../components/NotFoundPage.jsx";
+import {LoginPage} from "../components/LoginPage.jsx";
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header/>
             <Switch>
-                <Route exact path="/" component={ExpenseDashboardPage}/>
+                <Route exact path="/" component={LoginPage}/>
                 <Route path="/create" component={AddExpensePage}/>
+                <Route path="/dashboard" component={ExpenseDashboardPage}/>
                 <Route path="/edit/:id" component={EditExpensePage}/>
                 <Route path="/help" component={HelpPage}/>
                 <Route component={NotFoundPage}/>
